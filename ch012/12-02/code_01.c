73ba887630d78a648d269e11a454171375a9ef55
rkddmldyd <dlsxjsptdyd1234@gmail.com>
2026-09-08 17:14:29 +0900

    Create code_01.c

---
+++ ch012/12-02/code_01.c
@@ -0,0 +1,24 @@
+// **********************************************
+// 제 목 : 각 변수의 주소 출력
+// 날 짜 : 2026년 9월 8일
+// 작성자 : 2600172 정준석
+// **********************************************
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a = -100;
+    char b = 'A';
+    double c = 3.14;
+
+    int* pa = &a;
+    char* pb = &b;
+    double* pc = &c;
+
+    printf("int형 변수 a의 값은: %d\n", *pa);
+    printf("char형 변수 b의 값은: %c\n", *pb);
+    printf("double형 변수 c의 값은: %lf\n", *pc);
+
+    return 0;
+}
