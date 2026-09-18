d6de5537840e783ac00b61f0a32fc50a07718ff3
rkddmldyd <dlsxjsptdyd1234@gmail.com>
2026-09-18 19:23:49 +0900

    Improve ASCII case conversion
    Updated the program to convert uppercase letters to lowercase and vice versa directly in the input string, removing the need for a separate array.

--- ch013/13-02/code_02.c
+++ ch013/13-02/code_02.c
@@ -1,43 +1,35 @@
 // **********************************************
-// 제 목 : 문자열에서 대문자는 소문자로, 소문자로 대문자로 변경
-// 날 짜 : 2026년 9월 17일
-// 작성자 : 2600172정준석
+// 제 목 : 문자열의 대문자와 소문자 상호 변환
+// 날 짜 : 2026년 9월 18일
+// 작성자 : 2600172 정준석
 // **********************************************
 
 #define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable:6031)
 #include <stdio.h>
 
 int main(void)
 {
     char str[100];
     int i = 0;
-    char cstr[100];
-
     printf("문자열을 입력하시오: ");
-    scanf("%s", str);
+    if (scanf("%99s", str) != 1)
+        return 1;
 
     while (str[i] != '\0')
     {
         if (str[i] >= 'a' && str[i] <= 'z')
         {
-            cstr[i] = str[i] - 32;
+            str[i] = str[i] - ('a' - 'A');
         }
         else if (str[i] >= 'A' && str[i] <= 'Z')
         {
-            cstr[i] = str[i] + 32;
-        }
-        else
-        {
-            cstr[i] = str[i];
+            str[i] = str[i] + ('a' - 'A');
         }
 
         i++;
     }
 
-    cstr[i] = '\0';
-
-    printf("변환결과: %s", cstr);
+    printf("변환결과: %s\n", str);
 
     return 0;
 }
